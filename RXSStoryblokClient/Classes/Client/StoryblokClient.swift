84d4ca71fb6f532f191d563b05ee5d965ff951e4
Luís Lopes <luis.lopes@mindera.com>
2022-09-23 15:43:31 +0100

    Single story fetch fix

--- RXSStoryblokClient/Classes/Client/StoryblokClient.swift
+++ RXSStoryblokClient/Classes/Client/StoryblokClient.swift
@@ -13,8 +13,8 @@ public protocol StoryblokClient{
     init(forApiKey apiKey: String, withOAuthToken token: String, andSpaceId spaceId: String)
     func configureForContentDeliveryAccess(apiKey: String)
     func configureForManagementAccess(apiKey: String, oAuthToken: String, spaceId: String)
-    func fetchStory<T: StoryContent>(identifiedBy identifier: StoryIdentifier) async throws -> Story<T>?
-    func fetchStory<T: StoryContent>(identifiedBy identifier: StoryIdentifier, applying query: StoryblokSingleStoryQuery) async throws -> Story<T>?
+    func fetchStory<T: StoryContent>(identifiedBy identifier: StoryIdentifier) async throws -> SingleStory<T>?
+    func fetchStory<T: StoryContent>(identifiedBy identifier: StoryIdentifier, applying query: StoryblokSingleStoryQuery) async throws -> SingleStory<T>?
     func fetchStories<T: StoryContent>(applying query: StoryblokMultiStoryQuery) async throws -> Stories<T>?
     func createStory<T: StoryContent>(_ story: ManagementStory<T>) async throws -> ManagementStory<T>?
     func updateStory<T: StoryContent>(_ story: ManagementStory<T>) async throws -> ManagementStory<T>?
